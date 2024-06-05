8fc4b47a3fccb87374ea038a2511c4998350143c
ArshiaSamiezad <arshiasamiezad@gmail.com>
2024-06-05 22:16:51 +0330

    Main menu except pre game

--- src/main/java/view/Main/MainMenuViewController.java
+++ src/main/java/view/Main/MainMenuViewController.java
@@ -1,4 +1,33 @@
 package view.Main;
 
+import controller.ApplicationController;
+import javafx.scene.input.MouseEvent;
+import view.Login.LoginMenuView;
+import view.Profile.ProfileMenuView;
+
 public class MainMenuViewController {
+    public void goToPreGameMenu(MouseEvent mouseEvent) {
+
+    }
+
+    public void goToProfileMenu(MouseEvent mouseEvent) {
+        try {
+            new ProfileMenuView().start(ApplicationController.getStage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void logout(MouseEvent mouseEvent) {
+        ApplicationController.setLoggedInUser(null);
+        try {
+            new LoginMenuView().start(ApplicationController.getStage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void exitGame(MouseEvent mouseEvent) {
+        System.exit(0);
+    }
 }
