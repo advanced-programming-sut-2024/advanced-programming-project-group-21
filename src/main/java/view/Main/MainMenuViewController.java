cd0ae26d98fb59c39dc7920f362b078b5a0b17f8
Parsa <mp.hajighasemi@gmail.com>
2024-06-24 17:17:54 +0330

    Started Main Menu

--- src/main/java/view/Main/MainMenuViewController.java
+++ src/main/java/view/Main/MainMenuViewController.java
@@ -1,11 +1,27 @@
 package view.Main;
 
 import controller.ApplicationController;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
+import model.User.User;
 import view.Login.LoginMenuView;
 import view.Profile.ProfileMenuView;
 
 public class MainMenuViewController {
+    @FXML
+    private Button Start;
+    @FXML
+    private Button ProfileMenu;
+    @FXML
+    private Button Logout;
+    @FXML
+    private Button Exit;
+    @FXML
+    private Button Back;
+    @FXML
+    private Button Setting;
+
     public void goToPreGameMenu(MouseEvent mouseEvent) {
 
     }
@@ -28,6 +44,10 @@ public class MainMenuViewController {
     }
 
     public void exitGame(MouseEvent mouseEvent) {
+        User.saveUsersToJson();
         System.exit(0);
+    }
+
+    public void goToSettingMenu(MouseEvent mouseEvent) {
     }
 }
