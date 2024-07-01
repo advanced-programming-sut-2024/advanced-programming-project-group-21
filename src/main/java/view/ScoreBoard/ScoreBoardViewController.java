7afa4ea1fe89010f09b928ac7e1abbf522a459f7
Parsa <mp.hajighasemi@gmail.com>
2024-07-01 17:16:41 +0330

    scoreBoard logic

--- src/main/java/view/ScoreBoard/ScoreBoardViewController.java
+++ src/main/java/view/ScoreBoard/ScoreBoardViewController.java
@@ -4,9 +4,12 @@ import controller.ApplicationController;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import model.User.User;
 import view.Main.MainMenuView;
 import view.Main.MainMenuViewController;
 import view.UserInfo.UserInfoMenuView;
+
+import static java.lang.Math.min;
 
 public class ScoreBoardViewController {
     @FXML
@@ -71,7 +74,84 @@ public class ScoreBoardViewController {
     private Label wins10;
 
     public void initialize() {
+        int[] possible = new int[User.getUsers().size()];
+        int maxNumber = -1;
+        for (int i = 0; i < User.getUsers().size(); i++)
+            possible[i] = 1;
+        int size = min(10, User.getUsers().size());
+        for (int i = 0; i < size; i++) {
+            User max = null;
+            for (int j = 0; j < User.getUsers().size(); j++) {
+                if (max == null && possible[j] == 1) {
+                    max = User.getUsers().get(j);
+                    maxNumber = j;
+                } else {
+                    if (possible[j] == 1 && max.getHighestScore() < User.getUsers().get(j).getHighestScore()) {
+                        max = User.getUsers().get(j);
+                        maxNumber = j;
+                    } else if (possible[j] == 1 && max.getHighestScore() == User.getUsers().get(j).getHighestScore() && max.getWins() > User.getUsers().get(j).getWins()) {
+                        max = User.getUsers().get(j);
+                        maxNumber = j;
+                    }
+                }
+            }
+            possible[maxNumber] = 0;
+            String username = max.getUsername();
+            String score = String.valueOf(max.getHighestScore());
+            String wins = String.valueOf(max.getWins());
 
+            if (i == 0) {
+                username1.setText(username);
+                score1.setText(score);
+                wins1.setText(wins);
+
+            }
+            if (i == 1) {
+                username2.setText(username);
+                score2.setText(score);
+                wins2.setText(wins);
+            }
+            if (i == 2) {
+                username3.setText(max.getUsername());
+                score3.setText(String.valueOf(max.getHighestScore()));
+                wins3.setText(wins);
+            }
+            if (i == 3) {
+                username4.setText(max.getUsername());
+                score4.setText(String.valueOf(max.getHighestScore()));
+                wins4.setText(wins);
+            }
+            if (i == 4) {
+                username5.setText(max.getUsername());
+                score5.setText(String.valueOf(max.getHighestScore()));
+                wins5.setText(wins);
+            }
+            if (i == 5) {
+                username6.setText(max.getUsername());
+                score6.setText(String.valueOf(max.getHighestScore()));
+                wins6.setText(wins);
+            }
+            if (i == 6) {
+                username7.setText(max.getUsername());
+                score7.setText(String.valueOf(max.getHighestScore()));
+                wins7.setText(wins);
+            }
+            if (i == 7) {
+                username8.setText(max.getUsername());
+                score8.setText(String.valueOf(max.getHighestScore()));
+                wins8.setText(wins);
+            }
+            if (i == 8) {
+                username9.setText(max.getUsername());
+                score9.setText(String.valueOf(max.getHighestScore()));
+                wins9.setText(wins);
+            }
+            if (i == 9) {
+                username10.setText(max.getUsername());
+                score10.setText(String.valueOf(max.getHighestScore()));
+                wins10.setText(wins);
+            }
+        }
     }
 
     public void back(MouseEvent mouseEvent) {
