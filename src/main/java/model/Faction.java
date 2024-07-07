9ee99c31d98bcd89c3b8525b4641c2df81cbaf87
ArshiaSamiezad <arshiasamiezad@gmail.com>
2024-07-07 14:45:20 +0330

    deleted useless classes

--- src/main/java/model/Faction.java
+++ src/main/java/model/Faction.java
@@ -3,18 +3,8 @@ package model;
 import java.util.ArrayList;
 
 public class Faction {
-    private static ArrayList<Commander> commanders = new ArrayList<>();
     private static ArrayList<Card> cards = new ArrayList<>();
-    private static Commander currentCommander;
     private String name;
-
-    public static ArrayList<Commander> getCommanders() {
-        return commanders;
-    }
-
-    public static void setCommanders(ArrayList<Commander> commanders) {
-        Faction.commanders = commanders;
-    }
 
     public static ArrayList<Card> getCards() {
         return cards;
@@ -24,13 +14,6 @@ public class Faction {
         Faction.cards = cards;
     }
 
-    public static Commander getCurrentCommander() {
-        return currentCommander;
-    }
-
-    public static void setCurrentCommander(Commander currentCommander) {
-        Faction.currentCommander = currentCommander;
-    }
 
     public void doAbility() {
 
